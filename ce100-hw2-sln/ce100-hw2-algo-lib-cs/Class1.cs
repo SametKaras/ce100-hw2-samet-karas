db09948550c4ebec271437f290d2ce4998884904
SametKaras <sametkaras.tr@gmail.com>
2023-04-03 15:44:46 +0300

    Project Created.

---
+++ ce100-hw2-sln/ce100-hw2-algo-lib-cs/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ce100_hw2_algo_lib_cs
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ce100-hw2-sln/ce100-hw2-algo-test-cs/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ce100_hw2_algo_test_cs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
